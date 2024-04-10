7e29cfa382284dbb6cbc6339e584469b300f6c9d
yessenov <yessenov.zhassulan@gmail.com>
2024-04-10 20:51:14 +0600

    for ws

--- app/main.py
+++ app/main.py
@@ -137,7 +137,7 @@ async def websocket_endpoint(websocket: WebSocket,db:db_dependency):
 @listens_for(Models.Finances, "after_insert")
 def after_data_insert(mapper, connection, target):
     new_data = target
-
+    print(DTOtoJson(DTO(new_data)))
     manager.broadcast(DTO(new_data))
 def DTO(finances:Models.Finances)->WebSocketFinancesJson:
     temp = WebSocketFinancesJson(
